80d50d978a5ff45b28aab821a926c8cef0a17c7b
Bernardo <beefreguglia@gmail.com>
2024-06-28 05:29:42 -0300

    feature: create routes and controllers

--- src/server.js
+++ src/server.js
@@ -1,21 +1,9 @@
-const express = require("express"); // importar express
+const express = require("express"); 
+const routes = require("./routes");
 
-const app = express(); // Iniciar express
+const app = express();
+app.use(express.json());
+app.use(routes);
 
-//  Método GET
-app.get("/message/:id/:user", (request, response) => {
-  //  O request recebe as informações enviadas pelo front/cliente para fazer esta 
-  //  chamada, estamos recebendo o id como Route Params 
-  //  O response é a resposta enviada pela chamada
-  const { id, user } = request.params; 
-  response.send(`Id da mensagem: ${id} para o usuário ${user}`);
-})
-
-app.get("/users/", (request, response) => {
-  //Aqui já estamos recebemos os query params que são opcionais 
-  const { page, limit } = request.query;
-  response.send(`Página: ${page}. Mostrar ${limit}`);
-})
-
-const PORT = 3333; //Porta utilizada
-app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`)) // Deixar o sv ouvindo na porta utilizada
+const PORT = 3333;
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
