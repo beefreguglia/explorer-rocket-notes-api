171549d139058abe70f5515e6611887563aa20c9
Bernardo <beefreguglia@gmail.com>
2024-07-03 15:18:48 -0300

    feature: create show notes method

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -1,4 +1,4 @@
-const knex = require("../database/knex")
+const knex = require("../database/knex");
 
 class NotesController {
   async create(request, response) {
@@ -27,6 +27,19 @@ class NotesController {
     await knex("tags").insert(tagsInsert)
     
     return response.json();
+  }
+  async show(request, response) {
+    const { id } = request.params;
+
+    const note = await knex("notes").where({ id }).first();
+    const tags = await knex("tags").where({ note_id: id }).orderBy("name");
+    const links = await knex("links").where({ note_id: id }).orderBy("created_at");
+
+    return response.json({
+      ...note,
+      tags,
+      links,
+    })
   }
 }
 
